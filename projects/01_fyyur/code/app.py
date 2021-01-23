4a42c6f1f4caac8f6c10624959fdcd59a9ac4ff6
Mohammed Hany <47370413+Mohammed-Hani@users.noreply.github.com>
2021-01-24 01:30:21 +0200

    feat: Add the object returned from db insertion in the flash message

--- projects/01_fyyur/code/app.py
+++ projects/01_fyyur/code/app.py
@@ -250,15 +250,17 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  # insert form data as a new Venue record in the db, instead
+  # modify data to be the data object returned from db insertion
   error = False
+  deassociatedDict = {}
   try:
       req = request.form
       print(req['name'])
       venue = Venue(name=req['name'], city=req['city'], state=req['state'], address=req['address'],
                    phone=req['phone'], facebook_link=req['facebook_link'])
       venue.genres.extend(Genre.query.filter(Genre.name.in_(req.getlist('genres'))).all())
+      deassociatedDict = { name: venue.name}
       db.session.add(venue)
       db.session.commit()
   except:
@@ -268,12 +270,12 @@ def create_venue_submission():
   finally:
       db.session.close()
       if error == True:
-          flash('Venue ' + request.form['name'] + ' cannot be listed!')
+          flash('Venue ' + deassociatedDict['name'] + ' cannot be listed!', 'error')
           abort(500)
       else:
           # on successful db insert, flash success
-          flash('Venue ' + request.form['name'] + ' was successfully listed!')
-          # TODO: on unsuccessful db insert, flash an error instead.
+          flash('Venue ' + deassociatedDict['name'] + ' was successfully listed!')
+          # on unsuccessful db insert, flash an error instead.
           # e.g., flash('An error occurred.  Venue ' + data.name + ' could not be
           # listed.')
           # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
