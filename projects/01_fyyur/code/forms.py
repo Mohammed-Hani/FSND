d0671f176d1ceed6bf5f2f93ce22d887e80e2d32
Mohammed Hany <47370413+Mohammed-Hani@users.noreply.github.com>
2021-01-31 02:46:48 +0200

    refactor: Move States values and names to an enum

--- projects/01_fyyur/code/forms.py
+++ projects/01_fyyur/code/forms.py
@@ -4,6 +4,8 @@ from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
 from wtforms.validators import DataRequired, AnyOf, URL
 from appFactory import create_app
 from models import Genre
+from enums import State
+
 
 class SelectMultipleGenresField(SelectMultipleField):
     def __init__(self, label='', validators=None, **kwargs):
@@ -44,57 +46,7 @@ with app.app_context():
         name = StringField('name', validators=[DataRequired()])
         city = StringField('city', validators=[DataRequired()])
         state = SelectField('state', validators=[DataRequired()],
-            choices=[('AL', 'AL'),
-                ('AK', 'AK'),
-                ('AZ', 'AZ'),
-                ('AR', 'AR'),
-                ('CA', 'CA'),
-                ('CO', 'CO'),
-                ('CT', 'CT'),
-                ('DE', 'DE'),
-                ('DC', 'DC'),
-                ('FL', 'FL'),
-                ('GA', 'GA'),
-                ('HI', 'HI'),
-                ('ID', 'ID'),
-                ('IL', 'IL'),
-                ('IN', 'IN'),
-                ('IA', 'IA'),
-                ('KS', 'KS'),
-                ('KY', 'KY'),
-                ('LA', 'LA'),
-                ('ME', 'ME'),
-                ('MT', 'MT'),
-                ('NE', 'NE'),
-                ('NV', 'NV'),
-                ('NH', 'NH'),
-                ('NJ', 'NJ'),
-                ('NM', 'NM'),
-                ('NY', 'NY'),
-                ('NC', 'NC'),
-                ('ND', 'ND'),
-                ('OH', 'OH'),
-                ('OK', 'OK'),
-                ('OR', 'OR'),
-                ('MD', 'MD'),
-                ('MA', 'MA'),
-                ('MI', 'MI'),
-                ('MN', 'MN'),
-                ('MS', 'MS'),
-                ('MO', 'MO'),
-                ('PA', 'PA'),
-                ('RI', 'RI'),
-                ('SC', 'SC'),
-                ('SD', 'SD'),
-                ('TN', 'TN'),
-                ('TX', 'TX'),
-                ('UT', 'UT'),
-                ('VT', 'VT'),
-                ('VA', 'VA'),
-                ('WA', 'WA'),
-                ('WV', 'WV'),
-                ('WI', 'WI'),
-                ('WY', 'WY'),])
+            choices=State.choices())
         try:
             genres_choices = [(genre.name, genre.name) for genre in Genre.query.all()]
         except:
@@ -118,61 +70,11 @@ with app.app_context():
         except:
             genres_choices = []
         state = SelectField('state', validators=[DataRequired()],
-            choices=[('AL', 'AL'),
-                ('AK', 'AK'),
-                ('AZ', 'AZ'),
-                ('AR', 'AR'),
-                ('CA', 'CA'),
-                ('CO', 'CO'),
-                ('CT', 'CT'),
-                ('DE', 'DE'),
-                ('DC', 'DC'),
-                ('FL', 'FL'),
-                ('GA', 'GA'),
-                ('HI', 'HI'),
-                ('ID', 'ID'),
-                ('IL', 'IL'),
-                ('IN', 'IN'),
-                ('IA', 'IA'),
-                ('KS', 'KS'),
-                ('KY', 'KY'),
-                ('LA', 'LA'),
-                ('ME', 'ME'),
-                ('MT', 'MT'),
-                ('NE', 'NE'),
-                ('NV', 'NV'),
-                ('NH', 'NH'),
-                ('NJ', 'NJ'),
-                ('NM', 'NM'),
-                ('NY', 'NY'),
-                ('NC', 'NC'),
-                ('ND', 'ND'),
-                ('OH', 'OH'),
-                ('OK', 'OK'),
-                ('OR', 'OR'),
-                ('MD', 'MD'),
-                ('MA', 'MA'),
-                ('MI', 'MI'),
-                ('MN', 'MN'),
-                ('MS', 'MS'),
-                ('MO', 'MO'),
-                ('PA', 'PA'),
-                ('RI', 'RI'),
-                ('SC', 'SC'),
-                ('SD', 'SD'),
-                ('TN', 'TN'),
-                ('TX', 'TX'),
-                ('UT', 'UT'),
-                ('VT', 'VT'),
-                ('VA', 'VA'),
-                ('WA', 'WA'),
-                ('WV', 'WV'),
-                ('WI', 'WI'),
-                ('WY', 'WY'),])
+            choices=State.choices())
         phone = StringField(# implement validation logic for state
             'phone')
         web_link = StringField('web_link', validators=[URL()])
-        image_link = StringField('image_link')
+        image_link = StringField('image_link', validators=[URL()])
         genres = SelectMultipleGenresField(# implement enum restriction
             'genres', validators=[DataRequired()],
             choices=genres_choices)
