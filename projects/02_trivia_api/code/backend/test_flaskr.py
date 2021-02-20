f0ecdf724b0a03d08a8102138de348780a3b0d18
Mohammed Hany <47370413+Mohammed-Hani@users.noreply.github.com>
2021-02-20 02:59:54 +0200

    test: Add tests for /questions api endpoint

--- projects/02_trivia_api/code/backend/test_flaskr.py
+++ projects/02_trivia_api/code/backend/test_flaskr.py
@@ -30,9 +30,32 @@ class TriviaTestCase(unittest.TestCase):
         pass
 
     """
-    TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+        
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
+        self.assertTrue(len(data['categories']))
+        self.assertEqual(data['current_category'], -1)
+
+    def test_404_sent_requesting_beyond_valid_page(self):
+        res = self.client().get('/questions?page=1000', json={'cat': 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
+    
+    
+
 
 
 # Make the tests conveniently executable
