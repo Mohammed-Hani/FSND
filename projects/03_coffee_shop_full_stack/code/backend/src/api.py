fb7e6512e559b003442652f4913d567cfabc86df
Mohammed Hany <47370413+Mohammed-Hani@users.noreply.github.com>
2021-03-26 22:28:15 +0200

    fix: Add jwt argument to GET drinks api endpoint

--- projects/03_coffee_shop_full_stack/code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/code/backend/src/api.py
@@ -29,7 +29,7 @@ db_drop_and_create_all()
 '''
 @app.route('/drinks')
 @requires_auth('get:drinks-detail')
-def retrieve_drinks():
+def retrieve_drinks(jwt):
     drinks = Drink.query.order_by(Drink.id).all()
     drinks = [drink.short() for drink in drinks]
     if len(drinks) == 0:
