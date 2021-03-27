a4ecfd511b382aa698cd692f028d49b824c88a2d
Mohammed Hany <47370413+Mohammed-Hani@users.noreply.github.com>
2021-03-27 04:30:41 +0200

    chore: Add environment variables in frontend

--- projects/03_coffee_shop_full_stack/code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/code/frontend/src/environments/environment.ts
@@ -1,14 +1,12 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
+
 
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'authtenant.us.auth0.com', // the auth0 domain prefix
+    audience: 'coffeeShop', // the audience set for the auth0 app
+    clientId: 'PxV7ACkZpA8AwhpByXKuI61oYTMlwsOU', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
